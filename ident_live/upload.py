e36ad40bfee1d680a415f87b0225e851996b8076
energy-master <151200614+energy-master@users.noreply.github.com>
2025-03-04 11:21:33 +0000

    upload added

---
+++ ident_live/upload.py
@@ -0,0 +1,49 @@
+
+
+#!/usr/bin/env python3
+
+import requests, json, sys
+
+# API URL
+api_url = f'https://marlin-network.hopto.org/cgi-bin/upload_acoustic.php'
+
+# Parameters required for the API
+# filename and location values are read in from the command line and allows for batch runs
+
+filename    =   sys.argv[1] #must be full path (e.g. /home/user/data/_YYMMDD_HHMMSS_000.wav) Also note format of acoustic file
+location    =   sys.argv[2]
+user_uid    =   '001elen'
+api_key         =   '_marlin_ellen_001'
+
+# open the file stream
+file_stream = open(filename,'rb')
+
+# not required - standard headers
+headers = {}
+
+# declare post data
+post_data = {
+    'filename' : filename, 
+    'location' : location,
+    'api_key' : api_key,
+    'user_uid' : user_uid  
+}
+
+# create the file upload structure
+file_data = {
+    'upload_file' : file_stream
+}
+
+# send request
+r = requests.post(api_url,(post_data), headers, files=file_data)
+
+# read and print the response
+if r.json()['error'] == True:
+    print (r.json()['error-message'])
+else:
+    print (r.json()['log'])
+    
+
+
+
+
